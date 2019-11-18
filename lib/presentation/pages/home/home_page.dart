e08ccc477d1174ab64ea06b0affbafe187e5ff53
JHNLWHD <aljhoenilw@gmail.com>
2019-11-18 15:01:40 +0800

    :construction: WIP: Home and App Drawer

--- lib/presentation/pages/home/home_page.dart
+++ lib/presentation/pages/home/home_page.dart
@@ -1,32 +1,131 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gradient_app_bar/gradient_app_bar.dart';
 import 'package:zc_mobile_app/presentation/_bloc/authentication/authentication.dart';
 
 class HomePage extends StatelessWidget {
-  final String name;
+  final List<String> userInfo;
 
-  HomePage({Key key, @required this.name}) : super(key: key);
+  HomePage({Key key, @required this.userInfo}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.exit_to_app),
-            onPressed: () {
-              BlocProvider.of<AuthenticationBloc>(context).add(
-                LoggedOut(),
-              );
-            },
-          )
-        ],
+      appBar: GradientAppBar(
+        title: Text('ILoveZApp'),
+        gradient: LinearGradient(colors: [Colors.blue, Colors.purpleAccent])
+      ),
+      drawer: Drawer(
+        child: Column(
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [Colors.blue, Colors.purpleAccent]
+                  ),
+                ),
+                accountName: Text('${userInfo[0]}'),
+                accountEmail: Text('${userInfo[1]}'),
+                currentAccountPicture: Image(
+                  image: NetworkImage('${userInfo[2]}'),
+                ),
+            ),
+            Expanded(
+              child: MediaQuery.removePadding(
+                removeTop: true,
+                context: context,
+                child: ListView(
+                  children: <Widget>[
+                    ListTile(
+                        leading: Icon(Icons.home),
+                        title: Text("Newsfeed"),
+                        onTap: () {
+
+                        }
+                    ),
+                    ListTile(
+                        leading: Icon(Icons.call_to_action),
+                        title: Text("Mayor's Action Center"),
+                        onTap: () {
+
+                        }
+                    ),
+                    ListTile(
+                        leading: Icon(Icons.credit_card),
+                        title: Text("Citizen Card"),
+                        onTap: () {
+
+                        }
+                    ),
+                    ListTile(
+                        leading: Icon(Icons.report),
+                        title: Text("Citizen Report"),
+                        onTap: () {
+
+                        }
+                    ),
+                    ListTile(
+                        leading: Icon(Icons.view_list),
+                        title: Text("PESO Job Corner"),
+                        onTap: () {
+
+                        }
+                    ),
+                    ListTile(
+                        leading: Icon(Icons.place),
+                        title: Text("Places"),
+                        onTap: () {
+
+                        }
+                    ),
+                    ListTile(
+                        leading: Icon(Icons.report_problem),
+                        title: Text("SOS"),
+                        onTap: () {
+
+                        }
+                    ),
+                    ListTile(
+                        leading: Icon(Icons.camera_alt),
+                        title: Text("Zamboanga Moments"),
+                        onTap: () {
+
+                        }
+                    ),
+                    ListTile(
+                        leading: Icon(Icons.add),
+                        title: Text("Terms and Conditions"),
+                        onTap: () {
+
+                        }
+                    ),
+                    ListTile(
+                        leading: Icon(Icons.info_outline),
+                        title: Text("About"),
+                        onTap: () {
+
+                        }
+                    ),
+                    ListTile(
+                        leading: Icon(Icons.exit_to_app),
+                        title: Text("Logout"),
+                        onTap: () {
+                          BlocProvider.of<AuthenticationBloc>(context).add(
+                            LoggedOut(),
+                          );
+                        }
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        )
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: <Widget>[
-          Center(child: Text('Welcome $name!')),
+          Center(child: Text('Welcome ${userInfo[0]}!')),
         ],
       ),
     );
